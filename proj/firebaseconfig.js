2d6cece537a91300209e6b33eb79196030074fda
venkateshrajan2003 <113899273+venkateshrajan2003@users.noreply.github.com>
2022-09-19 15:56:14 +0530

    Add files via upload

---
+++ proj/firebaseconfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC8xxIRnpt1fXl5FzKkaaqyeEv7Hr5OYsc",
+    authDomain: "testingdb-e24d4.firebaseapp.com",
+    databaseURL: "https://testingdb-e24d4-default-rtdb.firebaseio.com",
+    projectId: "testingdb-e24d4",
+    storageBucket: "testingdb-e24d4.appspot.com",
+    messagingSenderId: "852187739607",
+    appId: "1:852187739607:web:ab821937e34c1ecda88792",
+    measurementId: "G-Y7S1JQY2NM"
+  };
+  firebase.initializeApp(firebaseConfig);
